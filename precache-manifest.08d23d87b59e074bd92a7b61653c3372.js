ce54ce0c6cd1ab1ce6aaae55defae468fabb5134
Rishabh Srivastava <srivastava829@gmail.com>
2020-09-25 04:06:50 +0530

    Updates

---
+++ precache-manifest.08d23d87b59e074bd92a7b61653c3372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453a4ba8db6ef7ebf126ff1e8a7fd1b3",
+    "url": "/air_pollution/index.html"
+  },
+  {
+    "revision": "381dc5e24abb500e909a",
+    "url": "/air_pollution/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "4135922dca55bf9a2d9b",
+    "url": "/air_pollution/static/js/2.03ec4c4e.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/air_pollution/static/js/2.03ec4c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "381dc5e24abb500e909a",
+    "url": "/air_pollution/static/js/main.4db668ca.chunk.js"
+  },
+  {
+    "revision": "3317e36bbedd21be7dec",
+    "url": "/air_pollution/static/js/runtime-main.ca1eca96.js"
+  }
+]);
